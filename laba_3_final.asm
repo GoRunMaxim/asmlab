767eb545c4471d49e527491df84a5b4d83e26293
GoRunMaxim <61494018+GoRunMaxim@users.noreply.github.com>
2020-05-27 07:59:54 +0300

    Update laba_3_final.asm

--- laba_3_final.asm
+++ laba_3_final.asm
@@ -1,260 +1,293 @@
-.MODEL small
-.STACK 100h  
+.model small
+.stack 100h
+.data      
+size equ 30
+msg db "Enter matrix...$"  
+massiv dw size dup(?)
+buffer db 7,7 dup('$')  
+error db 10,13,"Error! You entered unacceptable symbols", 10, 13," or number was overflowed, start AGAIN", 10, 13, '$'
+error_amount_less db 10,13, "amount should more than 0", 10,13,'$' 
+error_amount_more db 10,13, "amount should less than 31", 10,13,'$'   
+new_line db 10,13,'$'  
+new_element db 10,13,"Enter new element:$"
+base dw 10
+sizeCh db 3, ?, 3 dup (?), '$'
+sizeInt db ?
+NumberCh db 7, ?, 7 dup (?), '$'
+massInt dw 61, ?, 61 dup (?), '$'
+MinNumb dw 7FFFh  
+MaxNumb dw 8000h
+Amount dw ?  
+MinBorder dw ?
+MaxBorder dw ?
+OldRange dw ? 
+NewRange dw ?
+Divide dw ?
+dexs dw 10 
+double dw 2h
+CountMessage db "******Entering amount of array:******", 0Ah, 0Dh, '$'
+NumbersMessage db 0Ah, 0Dh, "******Entering your old array:******", 0Ah, 0Dh, '$'
+MinBorderMess db 0Ah, 0Dh, "Enter Minimal border", 0Ah, 0Dh, '$'
+MaxBorderMess db 0Ah, 0Dh, "Enter Maximal border", 0Ah, 0Dh, '$'
+MaxLessMin db 0Ah, 0Dh, "Max Border Cannot be less then Min Border" , 0Ah, 0Dh, '$' 
+OutPuting db 0Ah, 0Dh, "That is a result" , 0Ah, 0Dh, '$' 
+NewString db 0Ah, 0Dh, '$'
 
 .code
-;NewValue = (((OldValue - OldMin)*(NewMax-NewMin)/(OdlMax-OldMin)) + NewMin
-
-.MODEL small
-.STACK 100h  
-
-.code
-;NewValue = (((OldValue - OldMin)*(NewMax-NewMin)/(OdlMax-OldMin)) + NewMin
-
-;Readable version:
-
-;   OldRange = (OldMax-OldMin)     NewRange = (NewMax-NewMin)   
-;   NewValue = (OldValue - OldMin)*NewRange/OldRange) + NewMin
-OutInt proc    
-   test ax, ax
-   jns oi1
-   mov cx, ax
-   mov ah, 02h
-   mov dl, '-'
-   int 21h
-   mov ax, cx
-   neg ax   ;change sing
-oi1:  
-   xor cx, cx
-   mov bx, 10 
-oi2:
-   xor dx,dx
-   div bx
-   push dx
-   inc cx
-   test ax, ax
-   jnz oi2
-   mov ah, 02h
-oi3:
-   pop dx
-   add dl, '0'
-   int 21h
-   loop oi3 
-   ret
-OutInt endp
-
-
-ChToInt proc near 
-    xor ax, ax
-    xor bx, bx  
     
-    Bigne:
-    imul dexs
-    cmp dl, 0h
-    je OverFlow2:
-    mov ax, 0900h
-    mov dx, offset OverFlowed
-    int 21h
-    mov ax, 4C00h
-	int 21h	
-    OverFlow2:    
-    mov dl, bx+si+2
-    inc bx
-    cmp dl, 2Dh
-    je NotnowNeg: 
-    sub dl, 48
-    add ax, dx
-    cmp ax, 7FFFh
-    jle OverFlow1:
-    mov ax, 0900h
-    mov dx, offset OverFlowed
-    int 21h
-    mov ax, 4C00h
-	int 21h	
-    OverFlow1:
-    NotnowNeg:
-    mov dl, bx+si+2 
-    cmp dl, 0Dh
-    jne Bigne:
-    
-    cmp si+2, 2Dh
-    jne NegativeTr:
-        mov dx, 0FFFFh
-        sub dx, ax
-        inc dx
-        mov ax, dx
-    NegativeTr:
-    RET
-ChToInt endp
+print_string macro str
+    mov dx,offset str
+    mov ah, 09h
+    int 21h   
+    endm
 
-start:
-;*****Entering amount of numbers
-    mov ax, @data
-    mov ds, ax
-    mov ax, 0900h
-    mov dx, offset CountMessage     ;Amount of numbers
-    int 21h
-    mov ax, 0A00h
-    mov dx, offset sizeCh
-    int 21h
-    mov si, dx  
-    CALL ChToInt
-    mov sizeInt, al
-    cmp ax, 0001h 
-    jg not1:                  
-        mov ax, 4C00h
-	    int 21h
-    not1:
-    mov ch, 00h
-    mov cl, sizeInt
-    mov si, offset massInt+1  
-    mov [si], cx             
-    mov ax, 0900h
-    mov dx, offset NumbersMessage  ;elements of array
-    int 21h
-    xor bp, bp
-;************Scanning numbers of array    ;    
-    EnterNumb:                
-    mov ax, 0A00h
-    mov dx, offset NumberCh
-    int 21h
-    mov si, dx
-    CALL ChToInt
-    mov si, offset massInt+2
-    add si, bp
-    add bp, 2h
-    mov [si], ax
+get_number proc  
+input: 
+    mov dx, offset buffer
+    mov ah,0ah
+    int 21h 
+    
+    mov si,1
+    
+    mov di,2
+    xor ax,ax ;number in ax 
+    xor cx,cx
+    xor bx,bx
+    mov cl, buffer+1  
+
+buffer_cycle:    
+    cmp cl, buffer+1
+    jne get_digit
+     
+check_minus:
+    mov bl, buffer+2
+    cmp bl, '-'
+    jne get_digit
+    mov si,-1
+    inc di
+    loop buffer_cycle
+    
+get_digit: 
+    mov bl, buffer[di]
+    inc di
+    sub bl, '0'
+    jl error_input
+    add bl,'0'
+    sub bl,'9' 
+    jg error_input
+    add bl,'9'
+       
+create_number:
+    mul base
+    cmp ax, 8000h
+    ja error_input   
+    sub bx,'0'
+    add ax, bx
+    cmp ax, 8000h
+    ja error_input
+    loop buffer_cycle
+    imul si
+    cmp si, 1
+    je is_positive
+    ret    
+     
+is_positive: 
+    or ax,ax
+    js error_input
+    ret
+       
+error_input: 
+    print_string error
+    jmp Main            
+    ret
+get_number endp  
+
+get_numbers proc
+    mov cx,Amount
+    xor si,si
+matloop:
+    push cx
+    push si
+    print_string new_element
+    call get_number
+    pop si
+    pop cx
+    mov massiv[si],ax
     cmp ax, MinNumb
-    jg MinFinding:     
+    jg MinFinding:
     mov MinNumb, ax
     MinFinding:
     cmp ax, MaxNumb
-    jl MaxFinding:     
+    jl MaxFinding:
     mov MaxNumb, ax
     MaxFinding:
-    mov ax, 0900h
-    mov dx, offset NewString
-    int 21h
-    sub cx, 1h
-    cmp cx, 0h
-    jne EnterNumb:
-;**********Scanning Borders******;                 
-    mov [si]+2, '$'
+    inc si
+    inc si
+    loop matloop
+    ret
+get_numbers endp
+
+get_min proc
     mov ax, 0900h                   
     mov dx, offset MinBorderMess
-    int 21h   
-    mov ax, 0A00h
-    mov dx, offset NumberCh
     int 21h
-    mov si, dx    
-    CALL ChToInt 
-    mov si, offset MinBorder
-    mov [si], ax
-    
+    call get_number
+    mov MinBorder, ax
+    ret
+get_min endp 
+
+get_max proc
     mov ax, 0900h                   
     mov dx, offset MaxBorderMess
-    int 21h 
-    mov ax, 0A00h
-    mov dx, offset NumberCh
     int 21h
-    mov si, dx
-    CALL ChToInt 
-    mov si, offset MaxBorder
-    mov [si], ax
-    
-    cmp ax, MinBorder   ;compare low and high borders
-    jge MaxNotLess: 
-        mov ax, 0900h               
+    call get_number
+    mov MaxBorder, ax
+    cmp ax, MinBorder
+    jge MaxNotLess:
+        mov ax, 0900h
         mov dx, offset MaxLessMin
         int 21h
         mov ax, 4C00h
-	    int 21h
-MaxNotLess:
+        int 21h
+    MaxNotLess:
     mov ax, MaxBorder	;ax = 400
     sub ax, MinBorder	;ax = 400-200 = 200
     mov NewRange, ax	;NewRange = 200
-    mov Divide, ax	;Divide = 200
-    mov ax, MaxNumb	;ax = 5
-    sub ax, MinNumb	;ax = 5-1 = 4
-    mov OldRange, ax;OldRange = 4     
+    mov Divide, ax	    ;Divide = 200
+    mov ax, MaxNumb	    ;ax = 5
+    sub ax, MinNumb	    ;ax = 5-1 = 4
+    mov OldRange, ax    ;OldRange = 4     
     mov ax, NewRange
     mov dx, 0
     idiv OldRange
     mov Divide, ax
     xor bp, bp		
     mov ch, 00h		
-    mov cl, sizeInt	
-    NewNumbersInt:  	
-    mov si, offset massInt+2
-    add si, bp		
-    add bp, 2h		
-    mov ax, [si]	
+    mov cl, sizeInt
+    mov ax, Amount
+    mov ax, 0
+    mov si, ax
+    mov cx, 0	
+    NewNumbersInt:
+    mov ax, 0	
+    mov ax, massiv[si]	
     sub ax, MinNumb 
     imul Divide
     add ax, MinBorder
-    xor dx, dx			
-    test ax, ax		
-    mov [si], ax		
-    mov bx, dx		        
-    xor dx, dx   	
-    idiv double		
-    inc ax		    
-    cmp ax, bx		
-    jg Arrounding:	
-        mov ax, [si]	
-        inc ax		
-        mov [si], ax    
-    Arrounding:		
-    sub cx, 1h		
-    cmp cx, 0h		
-    jne NewNumbersInt: 	
+    cmp cx, Amount		
+    jg Arrounding:
+        mov massiv[si], ax		
+        inc si
+        inc si
+        inc cx
+        cmp cx, Amount
+    jne NewNumbersInt: 		   
+    Arrounding:					
+	ret  
+get_max endp
+
+
+get_amount proc
+    xor si,si
+    call get_number
+    mov Amount, ax
+    cmp ax, 0    
+    jg Min:
+    mov ax, 0900h
+    mov dx, offset error_amount_less
+    int 21h
+    mov ax, 4c00h 
+    int 21h
+    Min:
+    cmp ax, 31
+    jl Max: 
+    mov ax, 0900h
+    mov dx, offset error_amount_more
+    int 21h
+    mov ax, 4c00h 
+    int 21h
+    Max:
+    mov MaxNumb, ax 
+    
+    ret
+get_amount endp
+
+
+
+print_number proc
+    pusha
+    xor di, di
+    or ax, ax
+    jns Convert
+    push ax
+    mov dx, '-'
+    mov ah, 02h
+    int 21h
+    pop ax
+    neg ax
+convert:
+    xor dx, dx
+    div base; ostatok v dl
+    add dl, '0'
+    inc di
+    push dx
+    or ax, ax
+    jnz convert
+
+print:
+    pop dx
+    mov ah, 02h
+    int 21h
+    dec di
+    jnz print
+    popa
+    ret
+print_number endp
+
+print_massiv proc
     mov ax, 0900h                   
     mov dx, offset OutPuting    ;Results
-    int 21h   
-    
-    mov double, 0h
-    output:  
-    mov bx, offset massInt
-    mov ax, double
-    add ax, 2h
-    mov double, ax
-    add bx, double 
-    mov ax, [bx]
-    CALL OutInt
-    mov cx, massInt+1h 
-    mov ch, 0h 
-    sub cx, 1h
-    mov massInt+1, cx 
-    mov ax, 0900h                   
-    mov dx, offset NewString
+	int 21h
+    pusha
+    mov si, 0
+    mov di, 0
+    mov bx, dx
+    mov cx, 0
+
+print_el:
+    mov ax, massiv[si]
+    call print_number
+    mov ah, 02h
+    mov dl, ' '
     int 21h
-    cmp cx, 0
-    jne output:
+    mov di, 0
+    inc si
+    inc si
+    inc cx
+    cmp cx, Amount
+    jb print_el
+    popa
+    ret
+print_massiv endp
+
+Main:
+    mov ax, @data
+    mov ds, ax
+    mov dx, offset CountMessage
+    mov ah,9
+    int 21h  
+    call get_amount  
+    mov ax, 0900h
+    mov dx, offset NumbersMessage  ;elements of array
+    int 21h
+    call get_numbers
+    call get_min
+    call get_max
     
-    mov ax, 4C00h
-	int 21h   
-    
-.data       
-    sizeCh db 3, ?, 3 dup (?), '$'
-    sizeInt db ?
-    NumberCh db 7, ?, 7 dup (?), '$'
-    massInt dw 61, ?, 61 dup (?), '$'
-    MinNumb dw 7FFFh  
-    MaxNumb dw 8000h  
-    MinBorder dw ?
-    MaxBorder dw ?
-    
-    OldRange dw ?
-    NewRange dw ?
-    Divide dw ?
-    
-    dexs dw 10 
-    double dw 2h
-    OverFlowed db 0Ah, 0Dh, "OverFlow, watch out input", '$'
-    CountMessage db "Enter amount of numbers", 0Ah, 0Dh, '$'
-    NumbersMessage db 0Ah, 0Dh, "Enter elements:", 0Ah, 0Dh, '$'
-    MinBorderMess db 0Ah, 0Dh, "Enter Minimal border", 0Ah, 0Dh, '$'
-    MaxBorderMess db 0Ah, 0Dh, "Enter Maximal border", 0Ah, 0Dh, '$'
-    MaxLessMin db 0Ah, 0Dh, "Max Border Cannot be less then Min Border" , 0Ah, 0Dh, '$' 
-    OutPuting db 0Ah, 0Dh, "That is a result" , 0Ah, 0Dh, '$' 
-    NewString db 0Ah, 0Dh, '$'
-end start
+         
+    call print_massiv
+    mov ax, 4c00h 
+    int 21h    
+ends
+
+end Main
