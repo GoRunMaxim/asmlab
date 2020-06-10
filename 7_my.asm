c86edd54c8d2a7475f604f963bbb74e65f7c0139
GoRunMaxim <61494018+GoRunMaxim@users.noreply.github.com>
2020-06-10 14:05:59 +0300

    Update 7_my.asm

--- 7_my.asm
+++ 7_my.asm
@@ -1,228 +1,235 @@
-.model small
-.stack 100h
-.data
-sumbl db 1 dup(0)
-argc dw 0
-maxcmdsize equ 125
-cmd_length dw ?
-cmd_line db maxcmdsize+2,?,maxcmdsize dup(?)
-buf db maxcmdsize+2,?,maxcmdsize dup('$')  
-filename db 126 dup("$")
-progname db 126 dup("$")
-StringNumber db 0 
-localsize db 0
-StringFileOpenMsg db "File was opened ", 13,10, "$"
-StringFileCloseMsg db "File was closed ", 13,10, "$"
-StringErrorComandLineMsg db "Comand Line Error size",13,10,"$"
-StringFileErrorOpenMsg db "File Error Open", 13,10, "$"
-StringFileErrorCloseMsg db "File Error Close", 13,10, "$"
-StringErrorAllocatedMsg db "Error allocated memory", 13,10, "$"
-StringErrorStartMsg db "Error start other program", 13,10, "$"
-StringErrorCMDMsg db "CMD line is empty", 13,10,"$"
-StringBeginMsg db "The program has begun ", 13,10, "$"
-StringErrorParsMsg db "Error pars params in comand line",13,10,"$" 
-StringErrorOwerflowMsg db "Error Owerflow of size: size your entered bigger then 255",13,10,"$"
-StringEndMsg db "The program has end ", 13,10, "$"
-StringNewStr db 13,10,"$"
+.model tiny
+	.code
+	.186
+	org 100h	;com-program
+start:          
+	mov		ch, 0
+	mov		cl, es:[0080h]
+	mov		si, 81h
+	lea		di, programPath    
+	mov		bx, maxCmdSize
+	call	readParam	
+	lea		di, [commandText + 1]
+	mov		bx, 126
+	call	readParamWithSpaces    
+	
+	cmp		programPath[0], 0
+	je		invalidCommandLineArgs		
+	
+	call	calcCommandLineSize	           	             
+	             
+    mov		ah, 09h
+    mov		dx, offset message
+    int		21h
 
+    mov		ah, 0Ah
+    mov		dx, offset userInput
+    int		21h  
 
-EPB dw 0
-line db 125
-     db "/?"
-command_text db 122 dup(?)
-dsize=$-sumbl         
+    mov		si, offset string      
+    call	atoi  
+    cmp		error, 1
+    jne		loadProgram
+    mov		ah, 09h
+    mov		dx, offset invalidNumberMessage
+    int		21h
+    jmp		start    
     
-.code
-Print_str macro msg
-    push ax
-    push dx
-    mov ah,9
-    mov dx,offset msg
-    int 21h
-    pop dx
-    pop ax
-endm 
+loadProgram:              
+	mov 	ah, 09h
+    mov 	dx, offset newLine
+    int 	21h                              
+	mov 	sp, programLength + 100h + 200h	 ; перемещение стека на 200h после конца программы
+	mov 	ah, 4Ah							 ;освободить всю память после конца программы и стека
+	stackShift = programLength + 100h + 200h ;размер в параграфах +1
+	mov 	bx, stackShift shr 4 + 1
+	int 	21h 
+	; exec parameter block ( load and run )
+	mov 	ax, cs
+	mov 	word ptr EPB + 4, ax		; cmd segment
+	mov 	word ptr EPB + 8, ax		; fcb1 segment
+	mov 	word ptr EPB + 0Ch, ax		; fcb2 segment
+	xor 	cx, cx
+	mov 	cl, N_times
+runProgram:
+	mov 	ssSeg, ss
+	mov 	spSeg, sp
+	mov		ax, 4B00h
+	mov 	dx, offset programPath ;путь к файлу
+	mov 	bx, offset EPB		   ;блок epb
+	int 	21h					   ;запуск программы
+	jc 		errorDuringLoadingProgram
+	loop 	runProgram
+	mov 	ss, cs:ssSeg
+	mov 	sp, cs:spSeg
+    jmp 	exitStart
+errorDuringLoadingProgram:
+    mov 	ah, 09h
+    mov 	dx, offset error4Bh
+    int 	21h
+    jmp 	exitStart 
+invalidCommandLineArgs:
+    mov 	ah, 09h
+    mov 	dx, offset invalidCmdArgs
+    int 	21h
+exitStart:
+	int 	20h						;ret нельзя- стек перемещен
+proc	calcCommandLineSize
+		mov		cx, maxCmdSize
+		mov		al, 0Dh   
+		lea		di, commandText
+		repnz	scasb    
+		neg		cx        
+		dec		cx
+		add		cx, maxCmdSize
+		cmp		cx, 1
+		jne		cmdNotEmpty		                              
+		mov		cx, 0		   
+cmdNotEmpty:		                               
+		mov		commandLine, cl	                                     
+endp	calcCommandLineSize	                    
+               
+               
+proc	readParam		
+		push	ax	  
+		push	bx     	          	    
+		cmp		cx, 0	           
+	 	jle		exitReadParam 	      	                  	                  
+skipSpaces:
+		push	di
+		mov		di, si
+		mov		al, ' '
+		repz	scasb	   
+		dec		di   
+		inc		cx
+		mov		si, di  
+		pop		di   
+findParamEnd:
+		movsb     
+		dec		bx
+		dec		cx
+		cmp		bx, 0
+		je		paramEnded
+		cmp		byte ptr es:[di - 1], 0Dh
+		je		paramEnded
+		cmp		byte ptr es:[di - 1], ' '
+		jne		findParamEnd
+paramEnded:	   		
+		dec		di    
+		mov		byte ptr es:[di], 0
+		inc		di
+exitReadParam:	  
+		pop		bx   
+		pop		ax	     		        	
+		ret	            	
+endp	readParam  
+		                
+proc	readParamWithSpaces	
+		push	ax	
+		push	bx       	          	
+		cmp		cx, 0	      
+	 	jle		exitReadParam2	        	      	                  	                
+		        
+findParamEnd2:
+		movsb    
+		dec		cx    
+		dec		bx
+		cmp		bx, 0
+		je		paramEnded2
+		cmp		byte ptr es:[di - 1], 0Dh
+		je		paramEnded2
+		jmp		findParamEnd2
+		
+paramEnded2:	   		
+		dec		di    
+		mov		byte ptr es:[di], 0Dh
+		inc		di
 
-Makeparams proc
-    push si
-    push di
-    push dx
-    push cx
-    push bx
-    xor si,si
-    xor di,di   
-sravn:    
-    cmp cmd_line[si],' '
-    jne nextstep
-    call SkipSpaces
-    inc argc
-    cmp argc,2
-    je second
-    cmp argc,3
-    je forth
-nextstep:
-    mov dl,cmd_line[si]
-    mov buf[di],dl
-    inc di
-    inc si
-    xor dx,dx
-    mov dx,cmd_length
-    cmp si,dx
-    jne sravn
-    jmp forth 
-second:
-    mov cx,di
-    xor di,di
-secondcycle:
-    xor dx,dx
-    mov dl,buf[di]
-    mov filename[di],dl
-    mov buf[di],"$"
-    inc di
-    loop secondcycle
-    mov filename[di],0
-    xor di,di
-    jmp sravn
-forth:
-    mov cx,di
-    xor di,di
-forthcycle:
-    cmp buf[di],30h
-    jl ErrorEnded2
-    cmp buf[di],39h
-    jg ErrorEnded2
-    push ax
-    xor ax,ax
-    mov al,StringNumber
-    mul bx
-    mov StringNumber,al
-    pop ax
-    jc ErrorEnded1
-    push ax
-    xor ax,ax
-    mov al,buf[di]
-    sub al,30h
-    add StringNumber,al
-    pop ax
-    jc ErrorEnded1
-    inc di
-    loop forthcycle    
-    jmp EndMakeparams    
-ErrorEnded1:
-    pop cx
-    Print_str StringErrorOwerflowMsg
-    jmp ErrorEnds        
-ErrorEnded2:
-    Print_str StringErrorParsMsg
-ErrorEnds:
-    pop bx
-    pop cx    
-    pop dx    
-    pop si
-    pop di
-    jmp Exit                      
-EndMakeparams:
-    pop bx
-    pop cx    
-    pop dx    
-    pop si
-    pop di
-    ret
-Makeparams endp  
-    
-SkipSpaces proc
-SkipCycle:    
-    cmp cmd_line[si],' '
-    je skip
-    jmp EndSkip
-skip:
-    inc si
-    jmp SkipCycle
-EndSkip:    
-    ret
-SkipSpaces endp    
+exitReadParam2:	      
+		pop		bx
+		pop		ax	     		        	
+		ret	            	
+endp	readParamWithSpaces
 
-start:
-    mov ah,4Ah
-    mov bx,((csize/16)+1)+256/16+((dsize/16)+1)+256/16
-    int 21h
-    jc  ErrorExit0
-    mov ax, @data
-    mov es, ax
-    
-    xor cx,cx
-    mov cl,ds:[80h]
-    mov bx,cx
-    mov si,81h
-    mov di,offset cmd_line
-    rep movsb
-    
-    mov ds,ax
-    Print_str StringBeginMsg
-    mov cmd_length,bx
-    cmp cmd_length,0
-    je  ErrorExit00
 
-    call Makeparams    
-    
-    push cx
-    xor cx,cx
-    mov cl, StringNumber
-    xor ax, ax
-    mov ax, cx
-    cmp ax, 255
-    jg  badRange
-    cmp ax, 0
-    je badRange
 
-MakeCycle:    
-    call Run
-	loop MakeCycle
-    jmp Exit
-badRange:
-    Print_str StringErrorOwerflowMsg  
-    jmp    Exit
-ret
+	                
+atoi	proc
+;==== ds:si - string address ===  
+    	push bx
+    	push cx
+    	push dx
+    	push si  
+ 
+    	xor bx, bx
+    	xor dx, dx 
+    	mov error, 0
+    	
+getNumberLength:
+        xor cx, cx
+        mov di, offset length
+        mov cl, [di] 
+        cmp cl, 0
+        je invalidInput      	
+convert:	
+        xor ax, ax
+    	lodsb
+        cmp al, '-'	
+    	je invalidInput	
+        cmp al, '9'
+    	jnbe invalidInput    
+    	cmp al, '0'    
+    	jb invalidInput   
+    	sub ax, '0'	
+    	shl dx, 1	
+    	add ax, dx
+    	shl dx, 2	
+    	add dx, ax  
+    	cmp dx, MAX_NUMBER
+    	jg invalidInput 
+    	cmp dx, 0
+    	je invalidInput
+loop convert
+        jmp exit
 
-ErrorExit00: 
-    Print_str StringErrorCMDMsg
-    jmp Exit        
-ErrorExit0:
-    Print_str StringErrorAllocatedMsg
-    jmp Exit    
-ErrorExit1:    
-    Print_str StringErrorComandLineMsg
-    jmp Exit
-ErrorExit2:
-    Print_str StringFileErrorOpenMsg
-    jmp Exit
-ErrorExit3:
-    Print_str StringFileErrorCloseMsg
-    jmp Exit
-ErrorExit4:
-    Print_str StringErrorStartMsg               
-Exit:
-    Print_str StringEndMsg    
-    mov ax, 4c00h
-    int 21h
+invalidInput:
+        mov error, 1
+exit:   
+        mov ax, dx 
+        mov N_times, al             
+    	pop si
+    	pop dx 
+    	pop cx
+    	pop bx
+	    
+	    ret 
+atoi	endp 
+	
+	
+maxCmdSize		EQU		126
 
-Run proc
-    mov     ax, 4B00h
-    lea     dx, filename
-    lea     bx, EPB
-    int     21h
-    jc ErrorExit4 
-    ret
-Run endp
+message			db		'Emount of start?', 10, 13, ': $'     
+newLine         db      10, 13, '$' 
+invalidNumberMessage    db      'Invalid input: number should contain only digits in the following range [1, 255]', 10, 13, '$' 
+invalidCmdArgs          db      'Invalid command line input. Try again.', 10, 13, '$' 
+error4Bh                db      'Error has happened during loading/running program.', 10, 13, 'Check your program path and try again', '$'        
+error           db      0
+ssSeg			dw		0			; stack register keepers
+spSeg			dw		0
+userInput    	EQU $               ; Buffer for string
+				maxLength db 4      ; Maximum characters buffer can hold
+				length 	  db 0      ; Number of characters actually read,
+				string    db 4 dup ('$')          ; Actual characters read, including the final carriage return (0Dh)
+N_times			db		0	
+programPath		db		128 dup(0)			;asciz string
+				
+EPB				dw		0000				;текущее окружение
+				dw 		offset commandLine, 0	;адрес командной строки
+				dw 		005Ch, 0, 006Ch, 0	;Адреса FCB (File control block) программы
+				
+commandLine		db		maxCmdSize			;длина командной строки
+commandText		db 		' ', maxCmdSize dup(0Dh)	;командная строка
+MAX_NUMBER		equ		255
+programLength	equ		$ - start 
 
-Ended proc
-    mov ah,3Eh
-    int 21h
-    jc ErrorExit3 
-    Print_str StringNewStr
-    Print_str StringFileCloseMsg
-    ret
-Ended endp
-        
-csize=$-Makeparams    
 end start
